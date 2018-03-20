13c2df00bdff1e82ba1aca6805180bd90e7dcd32
Zimbe <zimbe.salonen@gmail.com>
2018-03-20 15:12:59 +0200

    pushku

---
+++ Assets/CombatScripts/PlayerCombatScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatScript : MonoBehaviour {
+
+	GameObject weapon;
+	int damage, weaponType, element, elementDamage;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
