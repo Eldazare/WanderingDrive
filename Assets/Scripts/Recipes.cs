4f528a0725685127fb4530419d0e861f8af29f7e
wibe1n <ville.takkinen@outlook.com>
2018-03-20 14:00:59 +0200

    alustavat classit
    alustavat craftasu classit

---
+++ Assets/Scripts/Recipes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipes<T> : MonoBehaviour {
+
+
+
+
+    public Crafting_Item Merge(List<Item> _list)
+    {
+        if (true)
+        {
+            //Luo uusi tavara
+        }
+
+        Crafting_Item _item = new Crafting_Item();
+        return _item;
+
+    }
+
+}
