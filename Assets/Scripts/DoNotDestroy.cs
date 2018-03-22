55e5c9413334ad39e75f1c4d9fc74768fbf19e97
Chainreaction <36271579+Chainreaction22@users.noreply.github.com>
2018-03-22 13:09:46 +0200

    v_0.0.3
    - Run, Victory and Defeat conditions no longer use seperate scenes.
    - The Encounter scene is now 3D, with balls attached to the player and the enemies.

---
+++ Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroy : MonoBehaviour {
+
+
+	public static DoNotDestroy self;
+
+
+	// Use this for initialization
+	void Awake () {
+		if (self == null) {
+			DontDestroyOnLoad (gameObject);
+			self = this;
+		} else if (self != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+
+
+}
--- Assets/Scripts/old/Enemy1.cs
+++ Assets/Scripts/old/Enemy1.cs
@@ -11,16 +11,5 @@ public class Enemy1 : MonoBehaviour {
 
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-
 
 }
