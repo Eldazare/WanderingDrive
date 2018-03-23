842cbada8618b3c49cfaa20d20d66573cb14401f
Elda <sakari.helokunnas@gmail.com>
2018-03-23 08:42:11 +0200

    running build - removed duplicates

--- Assets/Scripts/CombatScripts/EnemyStats.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnemyStats {
-	int ID, health, damage, element, elementWeakness, elementDamage, armorType;
-}
