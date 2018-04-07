511c8c92fd6bee91ded85690a6bdcd5cb74e3037
Elda <sakari.helokunnas@gmail.com>
2018-04-07 17:48:05 +0300

    WorldBuff abstraction in place

---
+++ Assets/_Scripts/ITEM/WorldBuffAbstraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBuffAbstraction {
+
+	public BuffType buffType;
+	public int duration; // in encounter
+
+
+	public WorldBuffAbstraction(BuffType type, int duration){
+		this.buffType = type;
+		this.duration = duration;
+	}
+}
---
+++ Assets/_Scripts/ITEM/WorldStatsContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStatsContainer {
+
+	public float health;
+	public float stamina;
+
+	private List<WorldBuffAbstraction> buffs;
+
+
+
+	public WorldStatsContainer(){
+		buffs = new List<WorldBuffAbstraction> ();
+	}
+
+	public void AddWorldBuff(WorldBuffAbstraction buff){
+		buffs.Add (buff);
+	}
+
+	public List<WorldBuffAbstraction> GetWorldBuffs(){
+		return buffs;
+	}
+
+
+	public void AfterEncounter(){
+		// decrease buff duration
+		foreach (WorldBuffAbstraction wba in buffs) {
+			wba.duration -= 1;
+			if (wba.duration <= 0) {
+				buffs.Remove (wba);
+			}
+		}
+	}
+}
