5cbc2cb88fb95cc9e7654510f480451d9ee5d0a2
Elda <sakari.helokunnas@gmail.com>
2018-04-13 14:02:32 +0300

    Added monospaced font: DejaVuSansMono | Fixed formatting in InfoBoxCreator - Works for standalone resolution

--- Assets/_Scripts/Data/InfoBoxCreator.cs
+++ Assets/_Scripts/Data/InfoBoxCreator.cs
@@ -11,8 +11,7 @@ public static class InfoBoxCreator  {
 				RecipeMaterial mat = recipe.materialList [i];
 				string materialName = mat.GetName ();
 				int inventoryAmount = Inventory.GetAmountInInventoryRecipMat (mat);
-				returnee += i + ": " + string.Format ("{0,-20}", materialName) + string.Format ("{0:00}", inventoryAmount) + "/" +
-					string.Format ("{0:00}", mat.amount);
+				returnee += string.Format ("{0,-20}{1,2}/{2,2}", materialName, inventoryAmount, mat.amount);
 			}
 			returnee += "\n";
 		}
@@ -23,14 +22,13 @@ public static class InfoBoxCreator  {
 
 	public static string GetRecipeUpgradeInfoString(RecipeUpgrade recipe){
 		string returnee = "";
-		returnee += "Upgrading: " + recipe.baseEquipment.GetName ()+"\n";
+		returnee += "Upgrading: " + recipe.baseEquipment.GetName ()+"\n\n";
 		for (int i = 0; i < 4; i++) {
 			if (i < recipe.materialList.Count) {
 				RecipeMaterial mat = recipe.materialList [i];
 				string materialName = mat.GetName ();
 				int inventoryAmount = Inventory.GetAmountInInventoryRecipMat (mat);
-				returnee += i + ": " + string.Format ("{0,-20}", materialName) + string.Format ("{0:00}", inventoryAmount) + "/" +
-					string.Format ("{0:00}", mat.amount);
+				returnee += string.Format ("{0,-20}{1,2}/{2,2}", materialName, inventoryAmount, mat.amount);
 			}
 			returnee += "\n";
 		}
@@ -40,33 +38,34 @@ public static class InfoBoxCreator  {
 	}
 
 	public static string GetMaterialInfoString(RecipeMaterial mat){
-		string returnee = "";
+		string returnee = mat.GetName() + "\n";
 		switch (mat.type) {
 		case ItemType.Wep:
 			WeaponStats block = WeaponCreator.CreateWeaponStatBlock ((WeaponType)System.Enum.Parse (typeof(WeaponType), mat.subtype.ToString ()), mat.itemId);
-			returnee += string.Format("{0,-20} {1,10} \n","Damage Type", block.weaknessType.ToString());
-			returnee += string.Format("{0,-20} {1,10} \n","Damage", block.damage); 
-			returnee += string.Format("{0,-20} {1,10} \n","Accuracy Bonus", block.accuracyBonus); 
-			returnee += string.Format("{0,-20} {1,10} \n","Element", block.element.ToString()); 
-			returnee += string.Format("{0,-20} {1,10} \n","Element Damage", block.elementDamage); 
-			returnee += string.Format("{0,-20} {1,10} \n","Armor", block.armorBonus); 
-			returnee += string.Format("{0,-20} {1,10} \n","Magic Armor", block.magicArmorBonus);  
-			returnee += string.Format("{0,-20} {1,10} \n","Block Modifier", block.blockModifier); 
-			returnee += string.Format("{0,-20} {1,10} \n","Dodge Modifier", block.dodgeModifier); 
+			returnee += string.Format("{0,-18} {1,7} \n","Damage Type", block.weaknessType.ToString());
+			returnee += string.Format("{0,-18} {1,7} \n","Damage", block.damage); 
+			returnee += string.Format("{0,-18} {1,7} \n","Accuracy Bonus", block.accuracyBonus); 
+			returnee += string.Format("{0,-18} {1,7} \n","Element", block.element.ToString()); 
+			returnee += string.Format("{0,-18} {1,7} \n","Element Damage", block.elementDamage); 
+			returnee += string.Format("{0,-18} {1,7} \n","Armor", block.armorBonus); 
+			returnee += string.Format("{0,-18} {1,7} \n","Magic Armor", block.magicArmorBonus);  
+			returnee += string.Format("{0,-18} {1,7} \n","Block Modifier", block.blockModifier); 
+			returnee += string.Format("{0,-18} {1,7} \n","Dodge Modifier", block.dodgeModifier); 
 			break;
 		case ItemType.Mat:
 			string matDesc = NameDescContainer.GetDescription (NameType.Mat, mat.itemId);
-
+			returnee += matDesc + "\n";
 			break;
 		case ItemType.Cons:
 			string consDesc = NameDescContainer.GetDescription((NameType)System.Enum.Parse(typeof(NameType), mat.subtype.ToString()), mat.itemId);
-
+			returnee += consDesc + "\n";
 			break;
 		case ItemType.Arm:
 			ArmorType aType = (ArmorType)System.Enum.Parse(typeof(ArmorType), mat.subtype.ToString());
 			if (aType == ArmorType.Accessory){
 				Accessory accessory = ArmorCreator.CreateAccessory(mat.itemId);
-
+				returnee += string.Format("{0,-18} {1,7} \n","Damage Bonus", accessory.damage);
+				returnee += string.Format("{0,-18} {1,7} \n","Damage Bonus", accessory.damage);
 			} else {
 				Armor armor = ArmorCreator.CreateArmor(aType, mat.itemId);
 
