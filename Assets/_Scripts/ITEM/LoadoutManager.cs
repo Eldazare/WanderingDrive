43109c7226b8b334b0a09544e31916a921ad1eb2
wibe1n <ville.takkinen@outlook.com>
2018-04-16 18:35:49 +0300

    Loadoutin hahmottelua
    Loadoutin hahmottelua

---
+++ Assets/_Scripts/ITEM/LoadoutManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LoadoutManager : MonoBehaviour {
+
+    public Image recipeImage;
+    public Text recipeInfo;
+    public Text resultInfo;
+    public RecipeMaterial currentMaterial;
+
+    public List<Sprite> spriteList = new List<Sprite>();
+
+    public int currentItem;
+    ItemSubType currentItemSubType;
+    ItemType currentItemType;
+
+    void Start () {
+		
+	}
+
+    private void ChooseHead() {
+        currentItemType = ItemType.Arm;
+        currentItemSubType = ItemSubType.Helm;
+    }
+
+    private void ChooseChest() {
+        currentItemType = ItemType.Arm;
+        currentItemSubType = ItemSubType.Chest;
+    }
+
+    private void ChooseLegs() {
+        currentItemType = ItemType.Arm;
+        currentItemSubType = ItemSubType.Legs;
+    }
+
+    private void ChooseArms() {
+        currentItemType = ItemType.Arm;
+        currentItemSubType = ItemSubType.Arms;
+    }
+
+    private void ChooseAccessory() {
+        currentItemType = ItemType.Arm;
+        currentItemSubType = ItemSubType.Accessory;
+    }
+
+    private void ChooseMainHand() {
+        currentItemType = ItemType.Wep;
+    }
+
+    private void ChooseOffHand() {
+        currentItemType = ItemType.Wep;
+    }
+
+    private void UpdateInfoTexts() {
+        currentMaterial = new RecipeMaterial(currentItemType, currentItemSubType, currentItem);
+        recipeInfo.text = InfoBoxCreator.GetMaterialInfoString(currentMaterial);
+        //resultInfo.text = InfoBoxCreator.GetMaterialInfoString(currentMaterial);
+    }
+}
--- Assets/_Scripts/ITEM/RecipeMaterial.cs
+++ Assets/_Scripts/ITEM/RecipeMaterial.cs
@@ -12,18 +12,29 @@ public class RecipeMaterial  {
 
 
 
-	public RecipeMaterial(string identifier){
-		string[] matArr = identifier.Split ("_".ToCharArray ());
-		type = (ItemType)System.Enum.Parse(typeof(ItemType),matArr [0]);
-		subtype = (ItemSubType)System.Enum.Parse(typeof(ItemSubType),matArr [1]);
-		itemId = int.Parse (matArr [2]);
-		if (matArr.Length > 3) {
-			amount = int.Parse (matArr [3]);
-		} else {
-			amount = 1;
-		}
-		itemName = NameDescContainer.GetName((NameType)System.Enum.Parse(typeof(NameType), subtype.ToString()), itemId);
-	}
+    public RecipeMaterial(string identifier)
+    {
+        string[] matArr = identifier.Split("_".ToCharArray());
+        type = (ItemType)System.Enum.Parse(typeof(ItemType), matArr[0]);
+        subtype = (ItemSubType)System.Enum.Parse(typeof(ItemSubType), matArr[1]);
+        itemId = int.Parse(matArr[2]);
+        if (matArr.Length > 3)
+        {
+            amount = int.Parse(matArr[3]);
+        }
+        else
+        {
+            amount = 1;
+        }
+        itemName = NameDescContainer.GetName((NameType)System.Enum.Parse(typeof(NameType), subtype.ToString()), itemId);
+    }
+
+    public RecipeMaterial(ItemType type, ItemSubType subtype, int id) {
+        this.type = type;
+        this.subtype = subtype;
+        this.itemId = id;
+        this.amount = 1;
+    }
 
 
 	public string GetIdentifier(){
