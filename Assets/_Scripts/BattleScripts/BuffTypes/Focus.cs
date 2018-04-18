bafd05f82c9e91b49738c9f0909871b29f3088f0
Zimbe <zimbe.salonen@gmail.com>
2018-04-18 19:25:06 +0300

    Focus, Overload, OverFocus buffs also shown in playerProfile

---
+++ Assets/_Scripts/BattleScripts/BuffTypes/Focus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : _Buff {
+
+	public Focus(int turns){
+		turnsRemaining = turns;
+	}
+
+	override public float DoYourThing(){
+		return 0;
+	}
+}
