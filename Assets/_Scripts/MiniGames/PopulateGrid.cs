7247ea54f30fd756c33f0f77b272c69aa0534537
wibe1n <ville.takkinen@outlook.com>
2018-05-03 17:08:32 +0300

    GatheringGame
    Gatheringpelin hamottelua

---
+++ Assets/_Scripts/MiniGames/PopulateGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateGrid : MonoBehaviour {
+
+    public GameObject groundSprite;
+    public int howMany;
+    public int depth;
+	void Start () {
+        howMany = 63;
+        Populate();
+	}
+
+    public void Populate() {
+        GameObject newObject;
+        for (int i = 0; i < howMany; i++) {
+            switch (depth) {
+                case 4:
+                    newObject = (GameObject)Instantiate(groundSprite, transform);
+                    break;
+            }
+            
+        }
+
+    }
+}
